b1693925d1efe9f0ebb5aae62861cb55968fbee2
김예지 <yjiu9992@gmail.com>
2021-09-28 09:14:54 +0900

    201814095-1

---
+++ Hello.java
@@ -0,0 +1,9 @@
+package git_exam1;
+
+public class Hello{
+
+    public static void main(String[] args){
+        System.out.println("201814095");
+        System.out.println("김예지");
+    }
+}
---
+++ World.java
@@ -0,0 +1,15 @@
+package git_exam1;
+
+public class World {
+    static int sum(int n){
+        int result = 0;
+        for (int i=0;i<=n;++i)
+            result += i;
+        return result;
+    }
+
+    public static void main(String[] args){
+        for(int i=5;i<=10;++i)
+            System.out.println(sum(i));
+    }
+}
